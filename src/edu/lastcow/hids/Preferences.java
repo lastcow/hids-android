986e4a105bdbc122f169b2014e9a0f7c99afcf31
zhijiang chen <zhijiang.chen@huirtech.com>
2013-04-23 16:45:04 -0400

    no message

---
+++ src/edu/lastcow/hids/Preferences.java
@@ -0,0 +1,17 @@
+package edu.lastcow.hids;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lastcow
+ * Date: 4/1/13
+ * Time: 2:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Preferences extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
